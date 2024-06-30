92a68632ad4b95dcc85c47b2e44d149250ad5f1a
Thangapandi-K <thangapandi.chella@gmail.com>
2024-06-30 13:59:18 +0530

    final output

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import products from './components/Products'
 import Navbar from './components/Navbar'
 import ProductPage from './components/ProductPage'
 import Footer from './components/Footer'
+import "./App.css"
 
 const App = () => {
+
+  // using useState for setting cart item count
+  const [cartItem, setCartItem] = useState(0);
+
+  
+ 
   return (
     <div>
-      <Navbar />
-      <div>
-        <ProductPage />
+      <Navbar 
+        cartItem={cartItem} // sending cart item value to navbar as props
+      />
+      <div className='container'>
+        {
+            products.map((product, index) =>(
+              <ProductPage 
+                key={index}
+                product={product} // sending product details to productpage as props
+                setCartItem={setCartItem} // sending setcart value to productpage as props to set cart value
+                cartItem={cartItem} // sending cart item value to productpage as props to handle change
+              />
+            ))
+        }
       </div>
       <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
-}
+const ProductPage = ({product, cartItem, setCartItem}) => {
 
-export default ProductPage
+    //using usestate for setting button properties
+    const [button,setButton] = useState('Add to Cart')
+  
+      // conclick event handler to setcart count and changing button properties
+      const handleClick = (e) => {
+          console.log(e)
+          if(e.target.innerText === 'Add to Cart')
+          {
+              setCartItem(cartItem + 1); // to add cart value
+              setButton('Remove from cart'); // to set button proprty after clicking
+          }
+          else{
+              setCartItem(cartItem - 1); // to remove cart value
+              setButton('Add to Cart'); // to reset button proprty after clicking
+          }
+        }
+    
+  
+    return (
+      <div className='container col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
+       
+              <div className='card'>
+                  <div className="card-header">
+                      <img src={product.image} alt="product-image" className="card-img-top"/>
+                  </div>
+                  <div className='card-body'>
+                      <h5 className='card-title'>Product: {product.name}</h5>
+                      <p className='card-text'>Description: {product.description}</p>
+                      <h5 className='card-subtitle mb-2 text-body-secondary'>Price: Rs.{product.price}</h5>
+                      <button className={`btn ${button === 'Add to Cart' ? 'btn-success' : 'btn-danger' }`} onClick={handleClick}>{button}</button>
+                  </div>
+              </div>
+  
+      </div>
+    )
+  }
+
+export default ProductPage;
